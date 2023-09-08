6283a68aa6fd81c1e3008869e55adf531291003f
jhayward01 <johnny@Johns-MacBook-Pro.local>
2023-09-08 12:17:55 -0700

    Adding Github actions.

--- test/itests.sh
+++ test/itests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Integration Test #1
-feclient | cut -f3- -d' ' | grep -v ^key > /tmp/test1.actual
+# feclient | cut -f3- -d' ' | grep -v ^key > /tmp/test1.actual
+feclient | cut -f3- -d' ' > /tmp/test1.actual
 diff /tmp/test1.actual test/test1.expect
