cc7a81ae8ed70901286ad334ef5db4bc21e9f80d
jhayward01 <johnny@Johns-MacBook-Pro.local>
2025-03-05 10:58:29 -0800

    Updating Go version to 1.22.

--- deployments/minikube/start_cluster.sh
+++ deployments/minikube/start_cluster.sh
@@ -8,7 +8,6 @@ eval $(minikube -p minikube docker-env)
 docker compose build
 
 # Load Kubernetes services and deployments
-# TODO think these paths are wrong
 minikube kubectl -- apply -f deployments/minikube/enc-server-go-db.yaml
 minikube kubectl -- apply -f deployments/minikube/enc-server-go-be.yaml
 minikube kubectl -- apply -f deployments/minikube/enc-server-go-fe.yaml
