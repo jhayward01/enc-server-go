32039d4c4ebc4aff4dc1b0bf2f2343adffe8f852
jhayward01 <johnny@Johns-MacBook-Pro.local>
2025-10-17 13:24:32 -0700

    Adding v2.

---
+++ pkg/v2-apis/fe/server/server.go
@@ -0,0 +1,62 @@
+// client
+package server
+
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+
+	"enc-server-go/pkg/utils"
+)
+
+type message struct {
+    ID     string  `json:"id"`
+    Record string  `json:"record"`
+}
+
+var messages = []message{
+    {ID: "1", Record: "Message1"},
+    {ID: "2", Record: "Message2"},
+    {ID: "3", Record: "Message3"},
+}
+
+func getMessages(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, messages)
+}
+
+type Server interface {
+
+	// Start server.
+	Start() (err error)
+}
+
+// Server implementation
+type serverImpl struct {
+	db       utils.DB
+}
+
+func (s *serverImpl) Start() (err error) {
+	
+    router := gin.Default()
+    router.GET("/messages", getMessages)
+
+    router.Run("localhost:8080")
+    
+    return err
+}
+
+func MakeServer(configs map[string]string) (s Server, err error) {
+
+	// Build data store wrapper.
+	db, err := utils.MakeDB(configs)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build server implementation.
+	si := &serverImpl{
+		db: db,
+	}
+
+	return si, nil
+}
