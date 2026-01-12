7e10bdac011bd7cde2f27f8ec78d4ce1505f9574
jhayward01 <johnny@Johns-MacBook-Pro.local>
2026-01-12 15:39:59 -0800

    Updating start scripts.

--- cmd/beclient/beclient.go
+++ cmd/beclient/beclient.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"enc-server-go/pkg/v1-sockets/be/client"
@@ -11,6 +12,11 @@ import (
 const configPath = "config/config.yaml"
 
 func main() {
+
+	// Comand line
+	var v2 bool
+	flag.BoolVar(&v2, "v2", false, "Run in v2 mode")
+	flag.Parse()
 
 	// Logging
 	logFile, err := utils.StartLog("beclient")
@@ -23,7 +29,7 @@ func main() {
 	// Load configuration file.
 	configs, err := utils.LoadConfigs(configPath)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 
 	// Load test params
@@ -33,26 +39,26 @@ func main() {
 	// Make client.
 	c, err := client.MakeClient(configs["beClientConfigs"])
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 
 	// Store record.
 	log.Println("record", string(record))
 	if err = c.StoreRecord(id, record); err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 
 	// Retrieve record.
 	retrieved, err := c.RetrieveRecord(id)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 	log.Println("retrieved", retrieved)
 
 	// Delete record.
 	err = c.DeleteRecord(id)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 	log.Println("deleted record")
 }
