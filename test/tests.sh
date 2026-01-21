be7d5f155edded959a7cfef81880ce9096110e05
jhayward01 <johnny@Johns-MacBook-Pro.local>
2026-01-21 15:00:16 -0800

    Updating cluster scripts.

--- test/tests.sh
+++ test/tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Integration Test #1
-feclient | cut -f3- -d' ' | grep -v ^key > /tmp/test1.actual
+feclient --v2 | cut -f3- -d' ' | grep -v ^key > /tmp/test1.actual
 diff /tmp/test1.actual test/test1.expect
