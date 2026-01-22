9fe1d80650aa4340e4178ba8c862ff2072c37598
jhayward01 <johnny@Johns-MacBook-Pro.local>
2026-01-22 11:26:01 -0800

    Updating cluster scripts.

--- test/tests.sh
+++ test/tests.sh
@@ -2,7 +2,7 @@
 
 # Integration Test #1
 feclient --v2 | cut -f3- -d' ' | grep -v ^key > /tmp/test1.actual
-sleep 10
+sleep 15
 docker logs enc-server-go-fe
 docker logs enc-server-go-be
 # docker logs enc-server-go-mongodb-1
