dded347502628aaad97e2594a21f9640a7260216
jhayward01 <jhayward01@users.noreply.github.com>
2026-08-13 13:33:34 -0700

    Updating logging system.

--- test/tests.sh
+++ test/tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Integration Test #1
-feclient --v2 | cut -c 44- > /tmp/test1.actual 
+# feclient --v2 | cut -c 44- > /tmp/test1.actual 
+feclient --v2 > /tmp/test1.actual 
 diff /tmp/test1.actual test/test1.expect
